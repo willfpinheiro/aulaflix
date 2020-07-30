fe63eae0fb4dbabdb33cb65f68a670bd302adf9f
willfpinheiro <42159903+willfpinheiro@users.noreply.github.com>
2020-07-30 17:36:04 -0300

    aula 4 metade

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -1,20 +1,108 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-function FormField({ label, type, name, values, placeholder, onChange }) {
-	return (
-		<div>
-			<label>
-				{label}
-				<input
-					type={type}
-					name={name}
-					value={values}
-					placeholder={placeholder}
-					onChange={onChange}
-				></input>
-			</label>
-		</div>
-	);
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type='color'] {
+    padding-left: 56px;
+  }
+`;
+const Label = styled.label``;
+
+Label.Text = styled.span`
+  color: #e5e5e5;
+  height: 57px;
+  position: absolute;
+  top: 0;
+  left: 16px;
+
+  display: flex;
+  align-items: center;
+
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+
+  transition: 0.1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585d;
+  color: #f5f5f5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585d;
+
+  padding: 16px 16px;
+  margin-bottom: 45px;
+
+  resize: none;
+  border-radius: 4px;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+
+  &:focus:not([type="color"]) + span {
+    transform: scale(0.6) translateY(-10px);
+  }
+
+  /*Template String*/
+  ${({ hasValue }) => hasValue && css`
+      &:not([type='color']) + span {
+        transform: scale(0.6) translateY(-10px);
+      }
+    `}
+`;
+
+function FormField({ label, type, name, value, onChange }) {
+  const fieldId = `id_${name}`;
+  const isTypeTextArea = type === 'textarea';
+  const tag = isTypeTextArea ? 'textarea' : 'input';
+
+  const hasValue = Boolean(value.length);
+  return (
+    <FormFieldWrapper>
+      <Label htmlFor={fieldId}>
+        <Input
+          as={tag}
+          id={fieldId}
+          type={type}
+          name={name}
+          value={value}
+          hasValue={hasValue}
+          onChange={onChange}
+        />
+        <Label.Text>{label}</Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
+
+FormField.defaultProps = {
+  type: ' text',
+  value: ' ',
+  onChange: () => { },
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,81 +1,78 @@
-import React, { useState } from "react";
-import PageDefault from "../../../components/PageDefault";
-import { Link } from "react-router-dom";
-import FormField from "../../../components/FormField";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-	const valoresIniciais = {
-		nome: "",
-		descricao: "",
-		cor: "",
-	};
-	const [categorias, setCategorias] = useState([]);
-	const [values, setValues] = useState(valoresIniciais);
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
 
-	function setValue(chave, valor) {
-		// chave: nome, descrição, cor
-		setValues({
-			...values,
-			[chave]: valor,
-		});
-	}
+  function setValue(chave, valor) {
+    // chave: nome, descrição, cor
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+  function handleChange(infosDoEvento) {
+    const { name, value } = infosDoEvento.target;
+    setValue(name, value);
+  }
+  return (
+    <PageDefault>
+      <h1>
+        cadastro de Categoria:
+        {values.nome}
+      </h1>
+      <form
+        style={{ background: values.cor }}
+        onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorias([...categorias, values]);
 
-	function handleChange(infosDoEvento) {
-		const { name, value } = infosDoEvento.target;
-		setValue(name, value);
-	}
+          setValues(valoresIniciais);
+        }}
+      >
+        <FormField
+          label="Nome da Categoria: "
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
 
-	return (
-		<PageDefault>
-			<h1>cadastro de Categoria: {values.nome}</h1>
-			<form
-				style={{ background: values.cor }}
-				onSubmit={function handleSubmit(infosDoEvento) {
-					infosDoEvento.preventDefault();
-					setCategorias([...categorias, values]);
+        <FormField
+          label="Descrição: "
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
 
-					setValues(valoresIniciais);
-				}}
-			>
-				<FormField
-					label="Nome da Categoria: "
-					type="text"
-					name="name"
-					value={values.nome}
-					placeholder="Nome"
-					onChange={handleChange}
-				/>
+        <FormField
+          label="Cor: "
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
 
-				<FormField
-					label="Descrição:"
-					type="textarea"
-					name="descricao"
-					value={values.descricao}
-					placeholder="Descrição"
-					onChange={handleChange}
-				/>
-
-				<FormField
-					label="Cor: "
-					type="color"
-					name="cor"
-					value={values.cor}
-					placeholder="cor"
-					onChange={handleChange}
-				/>
-
-				<button type="submit">Cadastrar</button>
-			</form>
-			<ul>
-				{categorias.map((categoria, indice) => {
-					return (
-						<li key={`${categoria}${indice}`}>{categoria.name}</li>
-					);
-				})}
-			</ul>
-			<Link to="/">Ir pra Home</Link>
-		</PageDefault>
-	);
+        <Button type="submit">Cadastrar</Button>
+      </form>
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>{categoria.nome}</li>
+        ))}
+      </ul>
+      <Link to="/">Ir pra Home</Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
